c2c1cbc4fc5db4db828a58b48377f488038f5434
Abdullah Al Mamun <aamamunszone@gmail.com>
2025-08-14 11:49:58 +0600

    done: problem-05 solved

---
+++ problem-05.js
@@ -0,0 +1,36 @@
+function resultReport(marks) {
+  if (Array.isArray(marks)) {
+    let total = 0;
+    for (let i = 0; i < marks.length; i++) {
+      total += marks[i];
+    }
+
+    let avg;
+    // The main reason for getting NaN earlier was that in the empty array ([]) case, division by zero occurred.
+    if (marks.length === 0) {
+      avg = 0;
+    } else {
+      avg = Math.round(total / marks.length);
+    }
+
+    let passCount = 0;
+    let failCount = 0;
+    for (let i = 0; i < marks.length; i++) {
+      if (marks[i] >= 40) {
+        passCount++;
+      } else {
+        failCount++;
+      }
+    }
+
+    return {
+      finalScore: avg,
+      pass: passCount,
+      fail: failCount,
+    };
+  } else {
+    return 'Invalid';
+  }
+}
+
+console.log(resultReport([]));
